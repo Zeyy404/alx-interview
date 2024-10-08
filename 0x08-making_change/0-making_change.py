56a0f49cc9b05d3a89ee27cac955b39b12f01b91
Zainab Salih <Zainab.Salih@outlook.com>
2024-10-08 21:16:12 +0400

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,18 +11,14 @@ def makeChange(coins, total):
     If total is 0 or less, return 0
     If total cannot be met by any number of coins you have, return -1
     """
-    num_coins = 0
-    if total == 0 or total < 0:
+    if total <= 0:
         return 0
+    
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
-    while total != 0:
-        max_coin = max(coins)
-        total = total - max_coin
-        if total < max_coin:
-            coins.remove(max_coin)
-        num_coins += 1
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-        if not coins:
-            return -1
-        elif total == 0:
-            return num_coins
+    return dp[total] if dp[total] != float('inf') else -1
