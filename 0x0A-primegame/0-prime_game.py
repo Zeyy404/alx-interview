fa0d766d25880216ee010d3a5250941af5cc70f3
Zainab Salih <Zainab.Salih@outlook.com>
2024-10-17 20:04:54 +0400

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,23 +3,24 @@
 
 
 def sieve_of_eratosthenes(max_num):
-        """
-        Function to find all primes up to
-        a maximum number using Sieve of Eratosthenes
-        """
-        is_prime = [True] * (max_num + 1)
-        p = 2
-        while (p * p <= max_num):
-            if is_prime[p]:
-                for i in range(p * p, max_num + 1, p):
-                    is_prime[i] = False
-            p += 1
-        return [p for p in range(2, max_num + 1) if is_prime[p]]
+    """
+    Function to find all primes up to
+    a maximum number using Sieve of Eratosthenes
+    """
+    is_prime = [True] * (max_num + 1)
+    p = 2
+    while (p * p <= max_num):
+        if is_prime[p]:
+            for i in range(p * p, max_num + 1, p):
+                is_prime[i] = False
+        p += 1
+    return [p for p in range(2, max_num + 1) if is_prime[p]]
 
 
 def isWinner(x, nums):
     """Returns: name of the player that won the most rounds"""
-    if x is None or nums is None or x == 0 or nums == []:
+    if ((x is None or nums is None or x == 0 or
+         not all(isinstance(n, int) and n > 0 for n in nums))):
         return None
 
     max_n = max(nums)
